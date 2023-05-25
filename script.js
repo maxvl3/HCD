adb13cada787a63a52f83f0c380a18723928c284
Max <maxvanliempdt@gmail.com>
2023-05-25 21:12:06 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,71 @@
+document.addEventListener("DOMContentLoaded", function () {
+  var uploadInput = document.getElementById("upload-input");
+
+  var uploadedImage = document.getElementById("uploaded-image");
+
+  uploadInput.addEventListener("change", function (e) {
+    var file = e.target.files[0];
+
+    var reader = new FileReader();
+
+    reader.onload = function (e) {
+      uploadedImage.src = e.target.result;
+    };
+
+    reader.readAsDataURL(file);
+
+    uploadInput.style.display = "none";
+
+    uploadedImage.style.display = "block";
+    document.getElementById("image-container").style.display = "block";
+  });
+});
+
+const openBlur = document.querySelector("#blur");
+const blurButtons = document.querySelector("#blur-buttons");
+const checkButton = document.querySelector("#check");
+
+openBlur.addEventListener("click", function() {
+    blurButtons.classList.toggle("active");
+});
+
+checkButton.addEventListener("click", function() {
+    blurButtons.classList.toggle("active");
+});
+
+
+
+
+
+// Vind de elementen
+const decreaseBlurButton = document.getElementById('decrease-blur');
+const increaseBlurButton = document.getElementById('increase-blur');
+const uploadedImage2 = document.getElementById('uploaded-image');
+
+// Stel de initiële blurwaarde in
+let blurAmount = 0;
+
+// Stel de minimale en maximale blurwaarden in
+const minBlurAmount = 0;
+const maxBlurAmount = 10;
+
+// Functie om de blurwaarde bij te werken
+function updateBlur() {
+    uploadedImage2.style.filter = `blur(${blurAmount}px)`;
+}
+
+// Event listener voor het verlagen van de blurwaarde
+decreaseBlurButton.addEventListener('click', function() {
+    if (blurAmount > minBlurAmount) {
+        blurAmount--;
+        updateBlur();
+    }
+});
+
+// Event listener voor het verhogen van de blurwaarde
+increaseBlurButton.addEventListener('click', function() {
+    if (blurAmount < maxBlurAmount) {
+        blurAmount++;
+        updateBlur();
+    }
+});
