08a577b7de0339ae27213b8bf2d0017aece201ab
Max <maxvanliempdt@gmail.com>
2023-05-25 21:20:28 +0200

    touchstart

--- script.js
+++ script.js
@@ -25,22 +25,18 @@ const openBlur = document.querySelector("#blur");
 const blurButtons = document.querySelector("#blur-buttons");
 const checkButton = document.querySelector("#check");
 
-openBlur.addEventListener("click", function() {
-    blurButtons.classList.toggle("active");
+openBlur.addEventListener("click", function () {
+  blurButtons.classList.toggle("active");
 });
 
-checkButton.addEventListener("click", function() {
-    blurButtons.classList.toggle("active");
+checkButton.addEventListener("click", function () {
+  blurButtons.classList.toggle("active");
 });
-
-
-
-
 
 // Vind de elementen
-const decreaseBlurButton = document.getElementById('decrease-blur');
-const increaseBlurButton = document.getElementById('increase-blur');
-const uploadedImage2 = document.getElementById('uploaded-image');
+const decreaseBlurButton = document.getElementById("decrease-blur");
+const increaseBlurButton = document.getElementById("increase-blur");
+const uploadedImage2 = document.getElementById("uploaded-image");
 
 // Stel de initiële blurwaarde in
 let blurAmount = 0;
@@ -51,21 +47,25 @@ const maxBlurAmount = 10;
 
 // Functie om de blurwaarde bij te werken
 function updateBlur() {
-    uploadedImage2.style.filter = `blur(${blurAmount}px)`;
+  uploadedImage2.style.filter = `blur(${blurAmount}px)`;
 }
 
 // Event listener voor het verlagen van de blurwaarde
-decreaseBlurButton.addEventListener('click', function() {
-    if (blurAmount > minBlurAmount) {
-        blurAmount--;
-        updateBlur();
-    }
+decreaseBlurButton.addEventListener("touchstart", function (event) {
+  event.preventDefault(); // Voorkom standaardgedrag van touchstart-gebeurtenis op mobiele apparaten
+
+  if (blurAmount > minBlurAmount) {
+    blurAmount--;
+    updateBlur();
+  }
 });
 
 // Event listener voor het verhogen van de blurwaarde
-increaseBlurButton.addEventListener('click', function() {
-    if (blurAmount < maxBlurAmount) {
-        blurAmount++;
-        updateBlur();
-    }
+increaseBlurButton.addEventListener("touchstart", function (event) {
+  event.preventDefault(); // Voorkom standaardgedrag van touchstart-gebeurtenis op mobiele apparaten
+
+  if (blurAmount < maxBlurAmount) {
+    blurAmount++;
+    updateBlur();
+  }
 });
